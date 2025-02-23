3e75dcfcc22190972dcb40f838faa8f952b706de
Álvaro Carrera Bernal <alvcarber1@alum.us.es>
2025-02-23 18:21:07 +0100

    feat: minor adjustments crew entity

--- src/main/java/acme/entities/crew/Crew.java
+++ src/main/java/acme/entities/crew/Crew.java
@@ -3,14 +3,16 @@ package acme.entities.crew;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-
-import org.hibernate.validator.constraints.Length;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,30 +21,36 @@ import lombok.Setter;
 @Setter
 public class Crew extends AbstractEntity {
 
+	// Serialisation version --------------------------------------------------
+
 	protected static final long		serialVersionUID	= 1L;
 
-	@NotBlank
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
 	@Column(unique = true)
-	@Pattern(regexp = "^[A-Z]{2,3}\\d{6}$", message = "{validation.code}")
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$", message = "{validation.code}")
 	protected String				code;
 
-	@NotBlank
-	@Pattern(regexp = "^\\+?\\d{6,15}$", message = "{validation.phoneNumber}")
+	@Mandatory
+	@ValidString(pattern = "^\\+?\\d{6,15}$", message = "{validation.phoneNumber}")
 	protected String				phoneNumber;
 
-	@NotBlank
-	@Length(max = 255)
+	@Mandatory
+	@ValidString
 	protected String				skills;
 
-	@NotNull
+	@Mandatory
+	@Valid
+	@Automapped
 	protected AvailabilityStatus	availability;
 
-	@NotBlank
-	protected String				airline;
-
-	@NotNull
+	@Mandatory
+	@ValidMoney
 	protected Money					salary;
 
-	protected int					yearsExperience;
+	@Optional
+	@ValidNumber(min = 0, integer = 2, fraction = 0)
+	protected Integer				yearsExperience;
 
 }
