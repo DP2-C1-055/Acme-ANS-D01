81ebfef48cf12a8704f27887fbf69303847854d2
elguille2 <guillermo2pacheco@gmail.com>
2025-03-10 17:06:58 +0100

    fix: fix validations in AirportEntity

--- src/main/java/acme/entities/flights/Leg.java
+++ src/main/java/acme/entities/flights/Leg.java
@@ -15,7 +15,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
-import acme.entities.airports.Airport;
+import acme.entities.airport.Airport;
 import lombok.Getter;
 import lombok.Setter;
 
